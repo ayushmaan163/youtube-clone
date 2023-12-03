e460f9e4ea35f6d0cb1da5f4e2c720ddf157346d
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-03 13:26:10 +0530

    Add Video Player configs & Youtube embedded videos

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,4 +1,37 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/SideBarSlice";
+import { useSearchParams } from "react-router-dom";
+
 const WatchPage = () => {
-  return <div>WatchPage</div>;
+  const [params] = useSearchParams();
+  // console.log(params.get("v"));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  return (
+    <div className="m-5 ">
+      {/* Video Player, description, comments */}
+      <div className="col-span-7">
+        <iframe
+          width="700"
+          height="400"
+          src={"https://www.youtube.com/embed/" + params.get("v")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+          className="rounded-xl"
+        ></iframe>
+      </div>
+
+      {/* Suggestions */}
+      <div className=""></div>
+    </div>
+  );
 };
 export default WatchPage;
