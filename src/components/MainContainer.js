3512e94ca5b2c601223df3d9d94e78c190af8ffd
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-03 11:52:37 +0530

    Add lock scroll functionality on SideBar opening

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,40 @@
+import { useSelector } from "react-redux";
 import ButtonList from "./ButtonList";
 import VideoContainer from "./VideoContainer";
+import { useEffect } from "react";
 
-const MainContainer = () => {
+const ScrollLockComponent = () => {
+  useEffect(() => {
+    // Save the current scroll position
+    const scrollPosition = window.scrollY;
+
+    // Disable scrolling on the body
+    document.body.style.overflow = "hidden";
+
+    // Restore the scroll position when the component is unmounted
+    return () => {
+      document.body.style.overflow = "auto";
+      window.scrollTo(0, scrollPosition);
+    };
+  }, []);
+
   return (
-    <div className="">
+    <div className="flex flex-col overflow-y-hidden">
       <ButtonList />
       <VideoContainer />
     </div>
+  );
+};
+
+const MainContainer = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  return !isMenuOpen ? (
+    <div className="flex flex-col">
+      <ButtonList />
+      <VideoContainer />
+    </div>
+  ) : (
+    <ScrollLockComponent />
   );
 };
 export default MainContainer;
