6923000497cbabba9033acf4d71eb8cd1fbc8611
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-03 11:51:09 +0530

    Filter buttons

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,4 +1,21 @@
+import Button from "./Button";
+
+const list = [
+  "All",
+  "Music",
+  "Computer Programming",
+  "Comedy",
+  "Web Development",
+  "Cricket",
+  "Cooking",
+  "Gaming",
+];
+
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex items-center p-2 mx-3">
+      <Button lists={list} />
+    </div>
+  );
 };
 export default ButtonList;
