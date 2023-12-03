d1efeef9a180edc9fe9da4829dfbb020bff80c13
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-03 11:48:30 +0530

    Card for Video thumbnails & other details

---
+++ src/components/VideoCard.js
@@ -0,0 +1,22 @@
+const VideoCard = ({ info }) => {
+  // const { snippet, statistics } = info;
+
+  return (
+    <div className="w-72 p-2 m-3 shadow-lg rounded-md">
+      <img
+        src={info?.snippet?.thumbnails?.medium?.url}
+        alt="thumbnail"
+        className="rounded-md"
+      />
+
+      <div className="font-bold py-2 flex flex-wrap">
+        {info?.snippet?.title}
+      </div>
+
+      <div>{info?.snippet?.channelTitle}</div>
+
+      <div>{info?.statistics?.viewCount}</div>
+    </div>
+  );
+};
+export default VideoCard;
