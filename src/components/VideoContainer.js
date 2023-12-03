f6831c7dca8d3182d868b3308da0f84805f4e4b9
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-03 11:47:44 +0530

    Container for Video Cards

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import { YOUTUBE_MOST_POPULAR_VIDEOS_API } from "../config/api";
+import VideoCard from "./VideoCard";
+
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  const [videos, setVideos] = useState([]);
+
+  const fetchVideos = async () => {
+    const data = await fetch(YOUTUBE_MOST_POPULAR_VIDEOS_API);
+    const json = await data.json();
+    // console.log(json.items);
+    setVideos(json.items);
+  };
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
+  return (
+    <div className="flex flex-wrap ">
+      {videos.map((video) => (
+        <VideoCard key={video.id} info={video} />
+      ))}
+    </div>
+  );
 };
 export default VideoContainer;
