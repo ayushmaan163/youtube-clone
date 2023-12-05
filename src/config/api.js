7f9b2fe0192c6c20e66014175e0ce037907f9b5b
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-05 11:45:11 +0530

    Add API for Youtube SearchBar Suggestions

--- src/config/api.js
+++ src/config/api.js
@@ -3,3 +3,9 @@ const GOOGLE_API_KEY = "AIzaSyByyJAp8jqjwq7cmR0txkrJ5OU3n95dc6A";
 export const YOUTUBE_MOST_POPULAR_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const YOUTUBE_SEARCH_API_II =
+  "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
