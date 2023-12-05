791af3f4bc97f63532f4b1e945805637a2b9a623
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-05 13:10:37 +0530

    Fixed the Header on top

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { YOUTUBE_SEARCH_API_II } from "../config/api";
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const getSearchSuggestions = async () => {
     // console.log(searchQuery);
@@ -36,75 +37,81 @@ const Header = () => {
   };
 
   return (
-    <div className="grid grid-flow-col shadow-lg py-3 px-2">
-      {/* Menu & Logo */}
-      <div className="flex items-center gap-5 ml-3 col-span-1 mr-7">
-        <i
-          class="fa-solid fa-bars fa-lg"
-          style={{ color: "#000000", cursor: "pointer" }}
-          onClick={() => toggleMenuHandler()}
-        ></i>
-
-        <div className="flex gap-1 items-center">
+    <div className="header">
+      <div className="grid grid-flow-col shadow-lg py-3 px-2">
+        {/* Menu & Logo */}
+        <div className="flex items-center gap-5 ml-3 col-span-1 mr-7">
           <i
-            class="fa-brands fa-youtube fa-2xl"
-            style={{ color: "#ff0000" }}
+            class="fa-solid fa-bars fa-lg"
+            style={{ color: "#000000", cursor: "pointer" }}
+            onClick={() => toggleMenuHandler()}
           ></i>
-          <Link to="/">
-            <span
-              className="text-2xl font-bold"
-              style={{ fontFamily: "Oswald" }}
-            >
-              YouTube
-            </span>
-          </Link>
-        </div>
-      </div>
 
-      {/* Search Bar */}
-      <div className="col-span-10 ">
-        <div>
-          <input
-            type="text"
-            placeholder="Search"
-            className="w-1/2 rounded-l-2xl py-1 px-3 border border-gray-700"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-
-          <button className="border border-gray-700 bg-gray-300 py-1 px-3 rounded-r-2xl">
+          <div className="flex gap-1 items-center">
             <i
-              class="fa-solid fa-magnifying-glass fa-md"
-              style={{ color: "#000" }}
+              class="fa-brands fa-youtube fa-2xl"
+              style={{ color: "#ff0000" }}
             ></i>
-          </button>
+            <Link to="/">
+              <span
+                className="text-2xl font-bold"
+                style={{ fontFamily: "Oswald" }}
+              >
+                YouTube
+              </span>
+            </Link>
+          </div>
         </div>
 
-        <div>
-          {searchQuery === "" ? null : (
-            <div className="z-10 fixed bg-white  flex justify-start items-center px-6 py-4 rounded-lg shadow-lg border border-gray-400">
-              <ul>
-                {suggestions.map((s) => (
-                  <li
-                    key={s}
-                    className="flex flex-wrap items-center gap-4 py-1 px-3 hover:bg-gray-300 hover:rounded-lg"
-                  >
-                    <i
-                      class="fa-solid fa-magnifying-glass fa-sm"
-                      style={{ color: "#000" }}
-                    ></i>
-                    {s}
-                  </li>
-                ))}
-              </ul>
+        {/* Search Bar */}
+        <div className="col-span-10 ">
+          <div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-1/2 rounded-l-2xl py-1 px-3 border border-gray-700"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onFocus={() => setShowSuggestions(true)}
+              onBlur={() => setShowSuggestions(false)}
+            />
+
+            <button className="border border-gray-700 bg-gray-300 py-1 px-3 rounded-r-2xl">
+              <i
+                class="fa-solid fa-magnifying-glass fa-md"
+                style={{ color: "#000" }}
+              ></i>
+            </button>
+          </div>
+
+          {showSuggestions && (
+            <div>
+              {searchQuery === "" ? null : (
+                <div className="z-10 fixed bg-white  flex justify-start items-center px-6 py-4 rounded-lg shadow-lg border border-gray-400">
+                  <ul>
+                    {suggestions.map((s) => (
+                      <li
+                        key={s}
+                        className="flex flex-wrap items-center gap-4 py-1 px-3 hover:bg-gray-300 hover:rounded-lg"
+                      >
+                        <i
+                          class="fa-solid fa-magnifying-glass fa-sm"
+                          style={{ color: "#000" }}
+                        ></i>
+                        {s}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           )}
         </div>
-      </div>
 
-      {/* Profile */}
-      <div className="col-span-1 flex justify-center items-center">
-        <i class="fa-solid fa-user" style={{ color: "#000000" }}></i>
+        {/* Profile */}
+        <div className="col-span-1 flex justify-center items-center">
+          <i class="fa-solid fa-user" style={{ color: "#000000" }}></i>
+        </div>
       </div>
     </div>
   );
