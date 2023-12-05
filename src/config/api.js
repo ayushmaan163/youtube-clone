369f4b20598b34b038a9eadf6e33a00b40195ede
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-05 12:03:39 +0530

    Update API URLs that reference http:// to https://

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,11 @@
 const GOOGLE_API_KEY = "AIzaSyByyJAp8jqjwq7cmR0txkrJ5OU3n95dc6A";
 
 export const YOUTUBE_MOST_POPULAR_VIDEOS_API =
-  "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_SEARCH_API_II =
-  "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
+  "https://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
