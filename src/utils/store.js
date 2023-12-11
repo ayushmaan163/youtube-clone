1776644ce9721fdd940173a95736a8a647759392
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-11 19:26:25 +0530

    Add SearchSlice in the Redux store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SideBarSlice from "./SideBarSlice";
+import SearchSlice from "./SearchSlice";
 
 const store = configureStore({
   reducer: {
     app: SideBarSlice,
+    search: SearchSlice,
   },
 });
 
