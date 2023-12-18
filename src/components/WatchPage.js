8a88f7a9b4256261e3b98c5278a3de97608180c2
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-18 22:29:42 +0530

    Add CommentsContainer to show comments below video player

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/SideBarSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [params] = useSearchParams();
@@ -14,23 +15,31 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="m-5">
-      {/* Video Player, description, comments */}
-      <div className="col-span-7">
+    <div className="m-8">
+      {/* Video Player */}
+      <div className="col-span-7 flex flex-col">
         <iframe
           width="100%"
-          height="600"
+          height="500"
           src={"https://www.youtube.com/embed/" + params.get("v")}
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
           className="rounded-xl"
         ></iframe>
+
+        {/* Description */}
+        {/* <div>Description</div> */}
+
+        {/* Comments */}
+        <div className="m-4 p-2">
+          <CommentsContainer />
+        </div>
       </div>
 
       {/* Suggestions */}
-      <div className=""></div>
+      {/* <div className="">Suggestions</div> */}
     </div>
   );
 };
